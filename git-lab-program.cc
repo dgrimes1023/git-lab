f7857a1f6c211b928c3ca5bcdb4c05c9b74a970b
Daniel <dg298321@ohio.edu>
2023-01-24 12:18:08 -0500

    commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Daniel Grimes>
+ *        Date: <1/24/23>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << end1;
+    return 0;
+}// main
+
